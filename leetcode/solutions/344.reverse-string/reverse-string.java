cbeddbe116100179ea50943d1f49de585db2e01d
Frank Zhuang <superfrankie621@gmail.com>
2018-01-18 19:21:24 +0800

    Site updated: 2018-01-18 19:21:21

--- leetcode/solutions/344.reverse-string/reverse-string.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public String reverseString(String s) {
-        StringBuffer sb = new StringBuffer();
-        for (int j = s.length() - 1; j >= 0; j--) {
-                sb.append(s.charAt(j));
-            }
-        return sb.toString();
-    }
-}
