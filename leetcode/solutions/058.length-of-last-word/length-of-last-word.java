d890f97d9e5580347b8b6b284a08c043954baef8
Latias94 <superfrankie621@gmail.com>
2018-06-18 11:43:28 +1000

    Site updated: 2018-06-18 11:43:27

--- leetcode/solutions/058.length-of-last-word/length-of-last-word.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
-    }
-}
--- leetcode/solutions/441.arranging-coins/arranging-coins.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public int arrangeCoins(int n) {
-        return (int) ((-1 + Math.sqrt(1 + 8 * (long) n)) / 2);
-    }
-}
